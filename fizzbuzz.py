c66c04d5c0ef973fa0a02c96323a22eb37883863
localgaji <eldorado829@naver.com>
2023-04-24 21:35:17 +0900

    feat: if else

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,9 @@
-def do_fizzbuzz():
-    print('hello')
+def do_fizzbuzz(num):
+    for i in range(1,num+1):
+        if i % 3 == 0:
+            print('fizz')
+        else:
+            print(i)
 
 if __name__ == '__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
